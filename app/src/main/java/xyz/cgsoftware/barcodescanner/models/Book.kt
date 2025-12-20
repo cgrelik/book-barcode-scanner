9a5c8c8500f3ba6e1afae0aff95f8fa54e73e5e0
Christopher Grelik <cgrelik@gmail.com>
2025-12-20 15:59:20 -0800

    Revert "Oauth sign in"

--- app/src/main/java/xyz/cgsoftware/barcodescanner/models/Book.kt
+++ app/src/main/java/xyz/cgsoftware/barcodescanner/models/Book.kt
@@ -3,8 +3,7 @@ package xyz.cgsoftware.barcodescanner.models
 class Book(val isbn13: String = "",
            val isbn10: String = "",
            val title: String = "",
-           val thumbnail: String? = "",
-           val id: String? = null) {
+           val thumbnail: String? = "") {
     override fun equals(other: Any?): Boolean {
         if (other is Book) {
             return this.isbn13 == other.isbn13
